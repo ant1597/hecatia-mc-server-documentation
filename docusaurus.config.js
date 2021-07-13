f0edc2e08a7f7b700fcbfbaf24e565acfab3046d
Ant 1597 <64364395+ant1597@users.noreply.github.com>
2021-07-13 20:29:28 +0000

    添加微博
    B 站动态停止更新日常内容
    --- #赫卡 Tia 的动态
    url: https://t.bilibili.com/547030317844712233
    datetime: 2021-07-13T19:17:01Z
    content: |
      🪐重新发一下公告吧，
      赫卡的水动态的动态一般都只会在微博了，
      b站只会发联动、视频、直播动态，
      原因是因为，很多水的动态会把公告、视频的动态埋掉，包括转发的fanart，也会被埋掉，大家就看不到这些优秀和重要的东西了。
      自己生活上的事情都是些无关紧要的小事，但是跟重要的东西一起发就会更吸引人注意，变得很重要一样，事实上只是些无聊的水水水。
      所以做出了这个决定，希望大家尊重，谢谢！
    ...

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,6 +62,10 @@ module.exports = {
               label: "Pixiv",
               href: "https://www.pixiv.net/artworks/90288006",
             },
+            {
+              lable: "Weibo",
+              href: "https://weibo.com/u/7363797389",
+            },
           ],
         },
         {
